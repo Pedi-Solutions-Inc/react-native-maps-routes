5a5baab7e20b211c87f5cec7eb4d45148b2e1402
Renz <git@mihou.dev>
2025-02-04 09:31:35 +0800

    Use a single client instead of creating many different ones
    Signed-off-by: Renz <git@mihou.dev>

--- src/api/google.ts
+++ src/api/google.ts
@@ -2,39 +2,38 @@ import {createClient, createRoute} from "@client.ts/core";
 import {LatLng} from "react-native-maps";
 import {GoogleRoutesResponse} from "../types/GoogleApi";
 
-export const createGoogleMapsRoutesClient = (apiKey: string) => {
-    return createClient("https://routes.googleapis.com", {
-        directions: {
-            prefix: "/directions",
-            routes: {
-                compute: createRoute<GoogleRoutesResponse>().dynamic((
-                    origin: LatLng,
-                    destination: LatLng,
-                    mode: "DRIVE" | "BICYCLE" | "TWO_WHEELER" | "WALK"
-                ) => {
-                    return {
-                        route: "POST /v2:computeRoutes",
-                        body: {
-                            origin: {
-                                location: {
-                                    latLng: origin,
-                                },
+export const googleMapsClient = createClient("https://routes.googleapis.com", {
+    directions: {
+        prefix: "/directions",
+        routes: {
+            compute: createRoute<GoogleRoutesResponse>().dynamic((
+                apiKey: string,
+                origin: LatLng,
+                destination: LatLng,
+                mode: "DRIVE" | "BICYCLE" | "TWO_WHEELER" | "WALK"
+            ) => {
+                return {
+                    route: "POST /v2:computeRoutes",
+                    body: {
+                        origin: {
+                            location: {
+                                latLng: origin,
                             },
-                            destination: {
-                                location: {
-                                    latLng: destination,
-                                },
+                        },
+                        destination: {
+                            location: {
+                                latLng: destination,
                             },
-                            travelMode: mode || "WALK",
                         },
-                        headers: {
-                            "Content-Type": "application/json",
-                            "X-Goog-Api-Key": apiKey,
-                            "X-Goog-FieldMask": "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline",
-                        },
-                    }
-                })
-            }
+                        travelMode: mode || "WALK",
+                    },
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-Goog-Api-Key": apiKey,
+                        "X-Goog-FieldMask": "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline",
+                    },
+                }
+            })
         }
-    })
-}
+    }
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { createGoogleMapsRoutesClient } from "./api/google";
+export { googleMapsClient } from "./api/google";
 export { MapViewRoute } from "./components/MapViewRoute";
 export { decodeRoutesPolyline } from "./utils/decoder";
